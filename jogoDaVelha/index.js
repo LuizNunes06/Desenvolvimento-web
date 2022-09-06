a529722029f933e70984b92570d729f7278f1428
Luiz Felipe Nunes <106554495+LuizNunes06@users.noreply.github.com>
2022-09-06 15:05:52 -0300

    Add files via upload

---
+++ jogoDaVelha/index.js
@@ -0,0 +1,42 @@
+let jogador = "X"
+
+const casa1 = document.getElementById("casa1")
+const casa2 = document.getElementById("casa2")
+const casa3 = document.getElementById("casa3")
+const casa4 = document.getElementById("casa4")
+const casa5 = document.getElementById("casa5")
+const casa6 = document.getElementById("casa6")
+const casa7 = document.getElementById("casa7")
+const casa8 = document.getElementById("casa8")
+const casa9 = document.getElementById("casa9")
+
+
+function jogada(casa) {
+    if (casa.innerHTML === "") {
+        casa.innerHTML = jogador;
+        alterna_jogador();
+        verifica_ganhador();
+    }
+}
+
+function alterna_jogador() {
+    if (jogador === "X") {
+        jogador = "O"
+    } else {
+        jogador = "X"
+    }
+}
+
+function verifica_ganhador() {
+    if (casa1.innerHTML != "" && casa1.innerHTML === casa2.innerHTML && casa2.innerHTML === casa3.innerHTML ||
+        casa1.innerHTML != "" && casa1.innerHTML === casa4.innerHTML && casa1.innerHTML === casa7.innerHTML ||
+        casa1.innerHTML != "" && casa1.innerHTML === casa5.innerHTML && casa1.innerHTML === casa9.innerHTML ||
+        casa2.innerHTML != "" && casa2.innerHTML === casa5.innerHTML && casa2.innerHTML === casa8.innerHTML ||
+        casa3.innerHTML != "" && casa3.innerHTML === casa6.innerHTML && casa3.innerHTML === casa9.innerHTML ||
+        casa3.innerHTML != "" && casa3.innerHTML === casa5.innerHTML && casa3.innerHTML === casa7.innerHTML ||
+        casa4.innerHTML != "" && casa4.innerHTML === casa5.innerHTML && casa4.innerHTML === casa6.innerHTML ||
+        casa6.innerHTML != "" && casa6.innerHTML === casa7.innerHTML && casa6.innerHTML === casa9.innerHTML) {
+        alert(`Vencedor ${casa1.innerHTML}`)
+
+    }
+}
